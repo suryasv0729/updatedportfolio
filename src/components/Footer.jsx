7d083f60f436f502af5f1ea11a80a20df9c3e146
Surya V <suryasv0729@gmail.com>
2024-07-14 18:22:43 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-4 text-secondary text-[14px] max-w-3xl leading-[30px] pl-10 ">
+      Design inspired by jsmastery.pro
+    </div>
+  );
+};
+
+export default Footer;
